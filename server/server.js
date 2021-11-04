f6c5b15c293a4711bf24c0056f6e6a65e6909076
kimkh0884 <86642919+kimkh0884@users.noreply.github.com>
2021-11-04 20:26:44 +0900

    Change server port

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-// 1. 서버 사용을 위해서 http 모듈을 http 변수에 담는다. (모듈과 변수의 이름은 달라도 된다.) 
-//express 모듈 불러오기
 const express = require("express");
 const mongoose = require("mongoose");
-//express 사용
+
 const app = express(); 
 
-const PORT = 8000;
+const PORT = 80;
 const MONGO_URI = "mongodb+srv://admin:admin12345@cluster0.pzm0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.use(express.static('public'));
@@ -24,5 +22,4 @@ app.get("/", (req, res) => {
     res.send("Server is running");
 });
 
-// http listen port 생성 서버 실행
 app.listen(PORT, () => console.log("Server is Running..."));
