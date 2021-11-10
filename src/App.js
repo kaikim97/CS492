05ab42f6c0dcf89057d23c7701fd84079e6372d5
Jaehan <jaehan@Jaehanui-MacBookPro.local>
2021-11-11 00:21:34 +0900

    add seats layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import "./App.css";
 import axios from "axios";
-import TimeTable from "./time/timeTable";
+import TimeTable from "./components/time/timeTable";
+import Seat from "./components/seat/seat";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-    <TimeTable />
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <TimeTable />
+        <Seat />
+    </div>
   );
 }
 
