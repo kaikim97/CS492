fad50e715248ae8d3c95cccc1cf901bdb7b3533a
chanijung <1016chani@kaist.ac.kr>
2021-11-14 22:47:46 +0900

    seats info added to hall

--- server/models/hall.js
+++ server/models/hall.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const hallSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    date: {type: String, required: true},
     time: {type: String, required: true},
-    available: {type: Number, required: true }
+    available: {type: Number, required: true }, // Number of available seats
+    occupied: {     // Map of preoccupied or reserved seats
+        type: Map,      // Key: seatID
+        of: {type: Boolean},     // Value: true(reserved), false(preoccupied)
+        default: {},
+        required: true
+     }
 },
 {
     timestamps:true
