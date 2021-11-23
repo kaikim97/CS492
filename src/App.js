16e4011cd8eeb7c4e4bddfba82c8693b88b27907
임세원 <imsewon@imsewon-ui-iMac.local>
2021-11-24 02:09:49 +0900

    before connecting server

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
+
 import logo from "./logo.svg";
 import "./App.css";
-import TimeTable from './time/timeTable'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import TopBar from "./components/TopBar";
+import MovieTable from "./components/MovieTable";
+import DateAndTimeTable from "./components/DateAndTimeTable";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-    <TimeTable />
+    <div className="App">
+      <TopBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<MovieTable />} />
+          <Route path="/movieInfo" element={<DateAndTimeTable />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
+
