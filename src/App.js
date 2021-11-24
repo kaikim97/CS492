bc7497743a3ffb71d148565b3def39611f1c8069
Jaehan <jaehan@Jaehanui-MacBookPro.local>
2021-11-25 00:55:52 +0900

    Dragging and Panning a layout & Make selectedseat state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -6,20 +5,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import MovieTable from "./components/MovieTable";
 import DateAndTimeTable from "./components/DateAndTimeTable";
+import Seat from "./components/seat/seat";
 
 function App() {
   return (
     <div className="App">
-      <TopBar />
+      {/* <TopBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<MovieTable />} />
           <Route path="/movieInfo" element={<DateAndTimeTable />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Seat />
     </div>
   );
 }
 
 export default App;
-
