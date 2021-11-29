50166cc99ae3c8853088bd0f9e1870e0704d6a08
임세원 <imsewon@imsewon-ui-iMac.local>
2021-11-30 04:32:23 +0900

    show info(naver open api) when hover movie

--- src/features/movieTable/MovieTable.js
+++ src/features/movieTable/MovieTable.js
@@ -12,16 +12,18 @@ import { useContext } from "react";
 import { AuthContext } from "../../context.js";
 import axios from "axios";
 
+const data = api.searchMovie("query=듄").then((response) => {
+  // const dune_director = response.data.items[0].director;
+  // const dune_actor = response.data.items[0].actor;
+  // const dune_userRating = response.data.items[0].userRating;
+});
+
 const movies = [
   { id: 0, name: "듄", poster: movie1 },
   { id: 1, name: "이터널스", poster: movie2 },
   { id: 2, name: "베놈2", poster: movie3 },
   { id: 3, name: "강릉", poster: movie4 },
 ];
-
-const data = api.searchMovie("query=듄").then((response) => {
-  console.log(response.data.items[0]);
-});
 
 const MovieTable = ({ movies = [] }) => {
   const context = useContext(AuthContext);
@@ -60,26 +62,65 @@ const MovieTable = ({ movies = [] }) => {
 
 const Movie = ({ movie, selected, setSelected }) => {
   const { id, name, poster } = movie;
+  const [hover, setHover] = useState(null);
+  const [userRating, setUserRating] = useState(null);
+  const [director, setDirector] = useState(null);
+  const [actor, setActor] = useState(null);
   return (
     <div
       class={` w-1/4 mr-poster pt-12 pb-6 px-poster border-2 
       ${selected === id ? "border-black" : "border-white"} rounded-lg bg-white 
-      ${selected === null ? "hover:border-black" : ""} focus:border-black `}
+       focus:border-black `}
       onClick={mouseClick}
     >
-      <div class="rounded-lg">
-        <img
-          className="poster"
-          src={poster}
-          class="object-contain w-full rounded-lg"
-        ></img>
+      <div class="group relative rounded-lg bg-gradient-to-t hover:bg-gray-300  hover:from-black hover:to-transparent ">
+        <div class="rounded-lg mix-blend-multiply z-10">
+          <img
+            className="poster"
+            src={poster}
+            class="object-contain w-full rounded-lg "
+            onMouseOver={movieHover}
+            onMouseLeave={movieLeave}
+          ></img>
+        </div>
+        <div class="opacity-0 group-hover:opacity-100 text-white text-xs font-semibold absolute bottom-0 p-5">
+          <p class="text-lg font-bold">
+            {userRating != null && `${userRating}`}
+          </p>
+          <p>{director != null && `${director} 감독`}</p>
+          <p>{actor != null && `${actor}`}</p>
+        </div>
       </div>
-      <div class="text-center mt-4 text-lg ">{name}</div>
+
+      <div class="text-center font-bold mt-4 text-lg ">{name}</div>
     </div>
   );
 
   function mouseClick(e) {
     setSelected(id);
+  }
+  function movieHover(e) {
+    setHover(id);
+    const data = api.searchMovie(`query=${name}`).then((response) => {
+      setUserRating(response.data.items[0].userRating);
+      const director_before_parse = response.data.items[0].director;
+      const actor_before_parse = response.data.items[0].actor;
+      const actor_after_parse = actor_before_parse
+        .substring(0, actor_before_parse.length - 1)
+        .split("|")
+        .join(" | ");
+      const director_after_parse = director_before_parse
+        .substring(0, director_before_parse.length - 1)
+        .split("|")
+        .join(" | ");
+      setDirector(director_after_parse);
+      setActor(actor_after_parse);
+    });
+  }
+  function movieLeave(e) {
+    setUserRating(null);
+    setDirector(null);
+    setDirector(null);
   }
 };
 
