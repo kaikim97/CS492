727772033de2356c4cc1e3b08fdc13e7070747ed
임세원 <imsewon@imsewon-ui-iMac.local>
2021-12-01 23:17:07 +0900

    before merge

--- src/features/dateAndTimeTable/DateAndTimeTable.js
+++ src/features/dateAndTimeTable/DateAndTimeTable.js
@@ -12,12 +12,18 @@ const DateAndTimeTable = () => {
   console.log("TITLE: ", title);
   return (
     <div class="w-1/3 h-95screen bg-white font-bold">
-      <div class="h-1/4">
-        <img
-          src={require(`../../components/movies/${title}.jpg`).default}
-          class="w-1/3 h-full object-cover w-full"
-        ></img>
+      <div class="h-1/4 relative bg-gradient-to-t bg-gray-300 from-black to-transparent ">
+        <div class="h-full mix-blend-multiply ">
+          <img
+            src={require(`../../components/movies/${title}.jpg`).default}
+            class="w-1/3 h-full object-cover w-full"
+          ></img>
+        </div>
+        <div class="text-white text-3xl font-bold absolute bottom-10 left-20">
+          <p>{title}</p>
+        </div>
       </div>
+
       <div class="flex h-3/4">
         <div class="flex-auto h-5/6 overflow-y-scroll">
           <DateTable />
